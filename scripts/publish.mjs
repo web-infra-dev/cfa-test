c544cacd1c4cf3fda6518fb31070e9a4a5944663
zjk <jingkai.zhao@foxmail.com>
2024-12-24 18:01:53 +0800

    chore: get npm token

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -1,9 +1,23 @@
 import { getOtp } from '@continuous-auth/client'
+import fs from 'fs/promises'
 import path from 'path'
 import { execa } from 'execa'
 
 async function publish() {
   const projectRoot = path.resolve(import.meta.dirname, '..')
+  const npmrcPath = path.resolve(projectRoot, '.npmrc')
+
+  if (process.env.NPM_TOKEN) {
+    // The npm token is used for identifying the user, not for publishing.
+    await fs.writeFile(
+      npmrcPath,
+      `//registry.npmjs.org/:_authToken = ${process.env.NPM_TOKEN}`,
+      'utf-8'
+    );
+    console.log(`Wrote NPM_TOKEN to ${npmrcPath}`);
+  } else {
+    throw new Error('NPM_TOKEN is not set')
+  }
 
   console.log('Getting OTP code')
 
@@ -11,7 +25,7 @@ async function publish() {
 
   console.log('OTP code get, continuing...')
 
-  const result = execa('npm', ['publish', '--otp', otp], {
+  const result = execa('npm', ['publish', '--registry', 'https://registry.npmjs.org', '--userconfig', npmrcPath, '--otp', otp], {
     cwd: projectRoot,
     env: process.env,
   })
